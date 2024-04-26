eb4f05cb90c6cf5d99b3eebfa6f67adefba4c27d
Tudor Morar <tudor.morar@multiversx.com>
2024-04-26 13:40:33 +0300

    Create combined store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,1 +1,9 @@
-export * from './models';
+import { combine } from 'zustand/middleware/combine';
+import { createStore } from 'zustand/vanilla';
+import { sessionNetworkStore } from './models';
+
+export const store = createStore(
+  combine(sessionNetworkStore, (sessionNetworkStore) => ({
+    ...sessionNetworkStore
+  }))
+);
