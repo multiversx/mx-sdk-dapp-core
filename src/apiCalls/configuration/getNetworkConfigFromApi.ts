eb0ceff0618d222c19c62addee20378424b5d0d6
Tudor Morar <tudor.morar@multiversx.com>
2024-05-01 10:24:21 +0300

    Migrate network related functions

---
+++ src/apiCalls/configuration/getNetworkConfigFromApi.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { getCleanApiAddress } from '../../store/slices/network/actions/getCleanApiAddress';
+import { NETWORK_CONFIG_ENDPOINT } from '../endpoints';
+import { ApiNetworkConfigType } from '../../types/network.types';
+
+const urlIsValid = (url: string) => {
+  try {
+    return Boolean(new URL(url));
+  } catch {
+    return false;
+  }
+};
+
+export async function getNetworkConfigFromApi() {
+  const apiAddress = getCleanApiAddress();
+
+  if (!urlIsValid(apiAddress)) {
+    return null;
+  }
+
+  const configUrl = `${apiAddress}/${NETWORK_CONFIG_ENDPOINT}`;
+
+  try {
+    const { data } = await axios.get<{
+      data: { config: ApiNetworkConfigType };
+    }>(configUrl);
+    if (data != null) {
+      return data?.data?.config;
+    }
+  } catch (err) {
+    console.error('error fetching configuration for ', configUrl);
+  }
+  return null;
+}
--- src/store/slices/index.ts
+++
@@ -1,2 +0,0 @@
-export * as account from './account';
-export * as network from './network';
