8378c7256d3976d6154296b603a322f8b0efaae0
Tudor Morar <tudor.morar@multiversx.com>
2024-05-16 13:17:39 +0300

    Update network slice

--- src/store/slices/network/network.ts
+++ src/store/slices/network/network.ts
@@ -1,14 +1,12 @@
-import { storage } from 'constants/storage';
 import { NetworkType } from 'types/network.types';
-import { devtools, persist } from 'zustand/middleware';
-import { immer } from 'zustand/middleware/immer';
-import { createStore } from 'zustand/vanilla';
 import { getKeys } from '../helpers/getKeys';
 import { getReactStore } from '../helpers/getReactStore';
 import { GetSetType } from '../helpers/types';
 import { listenToLogout } from '../shared/listenToLogout';
 import { emptyNetwork } from './emptyNetwork';
 import { NetworkSliceType } from './network.types';
+import { getActions } from '../helpers';
+import { getVanillaStore } from '../helpers/getVanillaStore';
 
 const initialData: NetworkSliceType = {
   ['network']: emptyNetwork,
@@ -29,62 +27,43 @@ type StateType = typeof initialState;
 
 const keys = getKeys(initialState);
 
-const definition = (set: GetSetType<StateType>): StateType => ({
-  ...initialData,
-  initializeNetworkConfig: (newNetwork) => {
-    const walletConnectV2RelayAddress =
-      newNetwork.walletConnectV2RelayAddresses[
-        Math.floor(
-          Math.random() * newNetwork.walletConnectV2RelayAddresses.length
-        )
-      ];
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { walletConnectV2RelayAddresses, ...rest } = newNetwork;
-    return set(
-      (state) => {
+const definition = (set: GetSetType<StateType>): StateType => {
+  const createActions = getActions({ set, actions });
+
+  return {
+    ...initialData,
+    ...createActions({
+      initializeNetworkConfig: (state, newNetwork) => {
+        const walletConnectV2RelayAddress =
+          newNetwork.walletConnectV2RelayAddresses[
+            Math.floor(
+              Math.random() * newNetwork.walletConnectV2RelayAddresses.length
+            )
+          ];
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { walletConnectV2RelayAddresses, ...rest } = newNetwork;
         state[keys.network] = {
           ...state[keys.network],
           ...rest,
           walletConnectV2RelayAddress
         };
       },
-      false,
-      { type: keys.initializeNetworkConfig }
-    );
-  },
-  setCustomWalletAddress: (customWalletAddress) =>
-    set(
-      (state) => {
+      setCustomWalletAddress: (state, customWalletAddress) => {
         state[keys.network].customWalletAddress = customWalletAddress;
-      },
-      false,
-      { type: keys.setCustomWalletAddress }
-    )
-});
+      }
+    })
+  };
+};
 
 const handleLogout = listenToLogout((state: StateType) => {
   state.setCustomWalletAddress('');
 });
 
-// vanilla store
-export const networkStore = createStore<StateType>()(
-  devtools(
-    persist(
-      immer((...a) => ({
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore:next-line
-        ...definition(...a),
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore:next-line
-        ...handleLogout(...a)
-      })),
-      {
-        name: 'networkStore',
-        storage
-      }
-    )
-  )
-);
+export const networkStore = getVanillaStore({
+  name: 'networkStore',
+  definition,
+  middleware: [handleLogout]
+});
 
 // react store
 export const useNetworkStore = getReactStore({
