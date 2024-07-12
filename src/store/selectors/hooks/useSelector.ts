1798e53c7bfe4fe2913dc51e709930ce42af1686
cipriandraghici <cipriandraghici93@gmail.com>
2024-07-12 12:57:57 +0300

    fix selectors in NextJS

--- src/store/selectors/hooks/useSelector.ts
+++ src/store/selectors/hooks/useSelector.ts
@@ -2,8 +2,6 @@ import { StoreType, useStore } from '../../store';
 
 type ExtractState<S> = S extends { getState: () => infer X } ? X : StoreType;
 
-export const useSelector = <T>(
-  selector: (state: ExtractState<StoreType>) => T
-) => {
+export function useSelector<T>(selector: (state: ExtractState<StoreType>) => T) {
   return useStore(selector);
-};
+}
