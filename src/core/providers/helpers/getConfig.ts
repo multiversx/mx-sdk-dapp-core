e1c5f7031bde84ff7ce94b7911cd9bac1b8836ac
Tudor Morar <tudor.morar@multiversx.com>
2024-12-03 19:10:54 +0200

    Use safwindow

--- src/core/providers/helpers/getConfig.ts
+++ src/core/providers/helpers/getConfig.ts
@@ -1,12 +1,13 @@
 import type { LedgerConnectModal } from '@multiversx/sdk-dapp-core-ui/dist/components/ledger-connect-modal';
 import { defineCustomElements } from '@multiversx/sdk-dapp-core-ui/loader';
+import { safeWindow } from 'constants/index';
 import {
   IProviderConfig,
   ProviderTypeEnum
 } from '../types/providerFactory.types';
 
 export const getConfig = async (config: IProviderConfig = {}) => {
-  defineCustomElements(window);
+  defineCustomElements(safeWindow);
   const ledgerModalElement = document.createElement(
     'ledger-connect-modal'
   ) as LedgerConnectModal;
