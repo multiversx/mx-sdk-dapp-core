582486876c2c26ce53b041218a42eeaa97c7b043
Tudor Morar <tudor.morar@multiversx.com>
2024-12-12 18:22:15 +0200

    Remove edited

--- src/core/providers/helpers/crossWindow/createCrossWindowProvider.ts
+++ src/core/providers/helpers/crossWindow/createCrossWindowProvider.ts
@@ -1,6 +1,4 @@
 import { isBrowserWithPopupConfirmation } from 'constants/browser.constants';
-import { safeWindow } from 'constants/window.constants';
-import { defineCustomElements } from 'lib/sdkDappCoreUi';
 import { CrossWindowProvider } from 'lib/sdkWebWalletCrossWindowProvider';
 import { networkSelector } from 'store/selectors/networkSelectors';
 import { getState } from 'store/store';
@@ -17,8 +15,6 @@ export async function createCrossWindowProvider({
   const network = networkSelector(getState());
   const provider = CrossWindowProvider.getInstance();
   await provider.init();
-
-  await defineCustomElements(safeWindow);
 
   provider.setWalletUrl(walletAddress || network.walletAddress);
   provider.setAddress(address);
