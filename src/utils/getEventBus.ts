3e16b3baf80990617eac01573ccc796162a121a9
Gavrila Andrei <andrei.gavrila@multiversx.com>
2024-12-13 12:58:32 +0200

    Add getEventBus func

---
+++ src/utils/getEventBus.ts
@@ -0,0 +1,23 @@
+import { IEventBus } from '@multiversx/sdk-dapp-core-ui/loader';
+import { safeWindow } from 'constants/index';
+import { defineCustomElements } from 'lib/sdkDappCoreUi';
+
+export const getEventBus = async <
+  T extends HTMLElement & { getEventBus: () => Promise<IEventBus | undefined> }
+>(
+  tagName: string
+) => {
+  await defineCustomElements(safeWindow);
+
+  const modalElement = document.createElement(tagName) as T;
+  document.body.appendChild(modalElement);
+  await customElements.whenDefined(tagName);
+
+  const eventBus = await modalElement.getEventBus();
+
+  if (!eventBus) {
+    throw new Error('Event bus not provided.');
+  }
+
+  return eventBus;
+};
