7930928ea73d12ee6c44df63ddc01115ca914df8
Gavrila Andrei <andrei.gavrila@multiversx.com>
2024-12-19 12:56:48 +0200

    Replace window with safeWindow

--- src/utils/createModalElement.ts
+++ src/utils/createModalElement.ts
@@ -2,15 +2,22 @@ import { IEventBus } from '@multiversx/sdk-dapp-core-ui/loader';
 import { safeWindow } from 'constants/index';
 import { defineCustomElements } from 'lib/sdkDappCoreUi';
 
+//TODO: allow override createModalElement similar to accountProvider and add typed name for all webcomponents
 export const createModalElement = async <
-  T extends HTMLElement & { getEventBus: () => Promise<IEventBus | undefined> }
+  T extends HTMLElement & {
+    getEventBus: () => Promise<IEventBus | undefined>;
+  }
 >(
   name: string
 ) => {
   await defineCustomElements(safeWindow);
 
-  const modalElement = document.createElement(name) as T;
-  document.body.appendChild(modalElement);
+  if (!safeWindow.document) {
+    return {} as T;
+  }
+
+  const modalElement = safeWindow.document.createElement(name) as T;
+  safeWindow.document.body.appendChild(modalElement);
   await customElements.whenDefined(name);
 
   return modalElement;
