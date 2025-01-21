612651d2b8be81306225640fe0d748cb72deb152
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-01-21 10:32:29 +0200

    Refactor LedgerProviderStrategy (#63)
    * Refactor LedgerProviderStrategy
    
    * Update changelog
    
    * Updates
    
    * Move comment

---
+++ src/core/providers/strategies/LedgerProviderStrategy/types/ledgerProvider.types.ts
@@ -0,0 +1,19 @@
+import { IProviderAccount } from '@multiversx/sdk-wallet-connect-provider/out';
+import { LedgerConnectStateManager } from 'core/managers';
+import { IEventBus } from 'types/manager.types';
+import { ILedgerConnectModalData } from './ledger.types';
+
+export type LedgerConfigType = {
+  version: string;
+  dataEnabled: boolean;
+};
+
+export type LedgerStateManagerType = LedgerConnectStateManager<
+  IEventBus<ILedgerConnectModalData>
+>;
+
+export type LedgerEventBusType = IEventBus<ILedgerConnectModalData>;
+
+export type LedgerLoginType = (options?: {
+  addressIndex: number;
+}) => Promise<IProviderAccount>;
