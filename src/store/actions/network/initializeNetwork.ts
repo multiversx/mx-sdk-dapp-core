095614706cf9ac9b1191b10bc6daf6f6791b035d
razvan.tomegea <razvan.tomegea@gmail.com>
2025-02-25 16:26:23 +0200

    Added network round duration initialization

--- src/store/actions/network/initializeNetwork.ts
+++ src/store/actions/network/initializeNetwork.ts
@@ -1,3 +1,4 @@
+import { getNetworkConfigFromApi } from 'apiCalls/configuration/getNetworkConfigFromApi';
 import { getServerConfiguration } from 'apiCalls/configuration/getServerConfiguration';
 import { fallbackNetworkConfigurations } from 'constants/network.constants';
 import { emptyNetwork } from 'store/slices/network/emptyNetwork';
@@ -38,16 +39,22 @@ export const initializeNetwork = async ({
   const fallbackApiAddress = fallbackConfig?.apiAddress;
 
   if (fetchConfigFromServer) {
-    const serverConfig = await getServerConfiguration(
-      customNetworkApiAddress || fallbackApiAddress
-    );
+    const apiAddress = customNetworkApiAddress || fallbackApiAddress;
+    const [dappConfig, networkConfig] = await Promise.all([
+      getServerConfiguration(apiAddress),
+      getNetworkConfigFromApi(apiAddress)
+    ]);
 
-    if (serverConfig != null) {
+    if (dappConfig != null) {
       const apiConfig: NetworkType = {
         ...fallbackConfig,
-        ...serverConfig,
+        ...dappConfig,
         ...customNetworkConfig
       };
+
+      if (networkConfig != null) {
+        apiConfig.roundDuration = networkConfig.erd_round_duration;
+      }
 
       initializeNetworkConfig(apiConfig);
       return apiConfig;
