5462bf5634f9d1c371565c3a6a8bfc5452989e57
razvan.tomegea <razvan.tomegea@gmail.com>
2025-03-07 15:12:30 +0200

    Build fix

--- src/utils/transactions/getTransactionsHistory/helpers/getTransactionAmount.ts
+++ src/utils/transactions/getTransactionsHistory/helpers/getTransactionAmount.ts
@@ -1,5 +1,5 @@
 import BigNumber from 'bignumber.js';
-import type { ITransactionAsset } from 'types/transaction-list-item.types';
+import { ITransactionAsset } from './getTransactionAssets';
 
 interface IGetTransactionAmountParams {
   transactionAssets: ITransactionAsset[];
