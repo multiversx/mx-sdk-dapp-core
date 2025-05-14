2ec8c7938a544fa01490e63d256e450435e2786b
Razvan Tomegea <razvantomegea@users.noreply.github.com>
2025-05-14 15:59:38 +0300

    Add dynamic gas station metadata retrieval (#166)
    * Add dynamic gas station metadata retrieval
    
    - Introduced `getGasStationMetadataFromApi` to fetch gas station data.
    - Implemented `initGasStationMetadata` to initialize gas station metadata in the app.
    - Updated `initApp` to call `initGasStationMetadata` based on the user's shard.
    - Modified `NetworkType` to accommodate new gas station metadata structure.
    - Updated CHANGELOG to reflect the addition of dynamic gas station functionality.
    
    * Refactor initApp and accountLogin to streamline gas station metadata initialization
    
    - Moved gas station metadata initialization into the accountLogin function.
    - Ensured that initGasStationMetadata is called conditionally based on the user's shard.
    - Improved code readability by adjusting indentation in initApp.
    
    * CHANGELOG
    
    * Refactor

--- src/core/providers/strategies/ExtensionProviderStrategy/ExtensionProviderStrategy.ts
+++ src/core/providers/strategies/ExtensionProviderStrategy/ExtensionProviderStrategy.ts
@@ -1,13 +1,13 @@
 import { Message, Transaction } from '@multiversx/sdk-core/out';
 import { ExtensionProvider } from '@multiversx/sdk-extension-provider/out/extensionProvider';
+import { providerLabels } from 'constants/providerFactory.constants';
 import { PendingTransactionsEventsEnum } from 'core/managers/internal/PendingTransactionsStateManager/types/pendingTransactions.types';
 
 import { IProvider } from 'core/providers/types/providerFactory.types';
-import { providerLabels } from 'constants/providerFactory.constants';
 import { ProviderErrorsEnum } from 'types/provider.types';
+import { BaseProviderStrategy } from '../BaseProviderStrategy/BaseProviderStrategy';
 import { getPendingTransactionsHandlers } from '../helpers/getPendingTransactionsHandlers';
 import { signMessage } from '../helpers/signMessage/signMessage';
-import { BaseProviderStrategy } from '../BaseProviderStrategy/BaseProviderStrategy';
 
 export class ExtensionProviderStrategy extends BaseProviderStrategy {
   private provider: ExtensionProvider | null = null;
