3e14208a353f08cc42239578b5d212ed57419930
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-05-15 17:13:14 +0300

    Update getCommonData tests

--- src/core/providers/strategies/helpers/signTransactions/helpers/getCommonData/tests/getCommonData.test.ts
+++ src/core/providers/strategies/helpers/signTransactions/helpers/getCommonData/tests/getCommonData.test.ts
@@ -36,6 +36,28 @@ describe('getCommonData', () => {
         feeLimit: '0.0001',
         gasLimit: '4200000',
         gasPrice: '1000000000',
+        decodedData: {
+          decimal: {
+            displayValue: 'NaN',
+            highlight: null,
+            validationWarnings: []
+          },
+          raw: {
+            displayValue: 'wrapEgld',
+            highlight: null,
+            validationWarnings: []
+          },
+          smart: {
+            displayValue: '',
+            highlight: null,
+            validationWarnings: []
+          },
+          text: {
+            displayValue: '',
+            highlight: null,
+            validationWarnings: []
+          }
+        },
         highlight: null,
         isEditable: true,
         needsSigning: true,
@@ -114,6 +136,28 @@ describe('when the gas limit is updated', () => {
         currentIndex: 0,
         currentIndexToSign: 0,
         data: 'wrapEgld',
+        decodedData: {
+          decimal: {
+            displayValue: 'NaN',
+            highlight: null,
+            validationWarnings: []
+          },
+          raw: {
+            displayValue: 'wrapEgld',
+            highlight: null,
+            validationWarnings: []
+          },
+          smart: {
+            displayValue: '',
+            highlight: null,
+            validationWarnings: []
+          },
+          text: {
+            displayValue: '',
+            highlight: null,
+            validationWarnings: []
+          }
+        },
         egldLabel: 'xEGLD',
         feeInFiatLimit: '$0.0036',
         feeLimit: '0.0002',
@@ -156,7 +200,7 @@ describe('when the gas limit is updated', () => {
     });
   });
 
-  it('should return the common data wit ppu for second transaction', async () => {
+  it('should return the common data with ppu for second transaction', async () => {
     const commonData = await getCommonData({
       ...mockData,
       currentScreenIndex: 3,
@@ -177,12 +221,41 @@ describe('when the gas limit is updated', () => {
         feeInFiatLimit: '$0.0998',
         feeLimit: '0.0056',
         gasLimit: '520000000',
+        decodedData: {
+          raw: {
+            displayValue:
+              'MultiESDTNFTTransfer@00000000000000000500139ed7ae4aa03792e6bcb332394a40fe746eefa47ceb@02@5745474c442d613238633539@@0de0b6b3a7640000@4d45582d613635396430@@e177704bc43f9bee3106@6164644c6971756964697479@0dbd2fc137a30000@df363e8872ed0d9235a7',
+            validationWarnings: [],
+            highlight:
+              '6164644c6971756964697479@0dbd2fc137a30000@df363e8872ed0d9235a7'
+          },
+          text: {
+            displayValue:
+              'MultiESDTNFTTransfer@\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0005\u0000\u0013�׮J�7�漳29J@�tn�|�@\u0002@WEGLD-a28c59@@\rඳ�d\u0000\u0000@MEX-a659d0@@�wpK�?��1\u0006@addLiquidity@\r�/�7�\u0000\u0000@�6>�r�\r�5�',
+            validationWarnings: [],
+            highlight: 'addLiquidity@\r�/�7�\u0000\u0000@�6>�r�\r�5�'
+          },
+          decimal: {
+            displayValue:
+              'MultiESDTNFTTransfer@122606984161813630683000759836492172880332059259659648235@2@27008948430895953375700137273@@1000000000000000000@364901398363583379235888@@1064735712967374589014278@30141412180830754292089058425@990000000000000000@1054088355837700843124135',
+            validationWarnings: [],
+            highlight:
+              '30141412180830754292089058425@990000000000000000@1054088355837700843124135'
+          },
+          smart: {
+            displayValue:
+              'MultiESDTNFTTransfer@erd1qqqqqqqqqqqqqpgqzw0d0tj25qme9e4ukverjjjqle6xamay0n4s5r0v9g@\u0002@WEGLD-a28c59@@1000000000000000000@MEX-a659d0@@1064735712967374589014278@addLiquidity@990000000000000000@1054088355837700843124135',
+            validationWarnings: [],
+            highlight:
+              'addLiquidity@990000000000000000@1054088355837700843124135'
+          }
+        },
         gasPrice: '1069322720',
         highlight:
           '6164644c6971756964697479@0dbd2fc137a30000@df363e8872ed0d9235a7',
         isEditable: true,
         needsSigning: true,
-        ppu: 11_760_000,
+        ppu: 11760000,
         ppuOptions: [
           {
             label: 'Standard',
@@ -190,11 +263,11 @@ describe('when the gas limit is updated', () => {
           },
           {
             label: 'Fast',
-            value: 11_760_000
+            value: 11760000
           },
           {
             label: 'Faster',
-            value: 19_287_760
+            value: 19287760
           }
         ],
         receiver:
